6603cdb25fd65afd481700243f6d772ddf404255
david <dcarte51@msudenver.edu>
2024-02-21 17:38:41 -0700

    add context

---
+++ src/context/users.js
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+import axios from "axios";
+const bcrypt = require('bcrypt');
+
+const UsersContext = createContext();
+
+function UsersProvider({ children }) {
+
+    const [user, setUser] = useState({})
+
+    // const [auth, setAuth] = useState({
+    //     username,
+    //     password
+    // })
+
+    const createUser = async (user) => {
+        const response = await axios.post("http://localhost:8080/users/register", user)
+    }
+    // auth is a username and password
+    //TODO: set up GET method in backend to take a username and password
+    // const getUser = async (auth) => {
+    //     const response = await axios.get(``)
+    // }
+}
+
+export { UsersProvider }
+
+export default UsersContext;
